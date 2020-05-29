c6ddac43d2267b8c077670828e8489170cb138d2
Socheat Sok <alex@socheat.net>
2020-05-29 10:52:28 +0700

    feat(stories): update example button story

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,12 +1,30 @@
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+
 import MyButton from './MyButton';
 
 export default {
   title: 'Button',
   component: MyButton,
+  decorators: [withKnobs]
 };
+
+// Knobs as dynamic variables.
+export const exampleWithKnobs = () => ({
+  components: { MyButton },
+  props: {
+    isDisabled: {
+      default: boolean('Disabled', false)
+    },
+    text: {
+      default: text('Text', 'Hello Storybook')
+    }
+  },
+  template: `<MyButton :isDisabled="isDisabled" @click="action(text)">{{ text }}</MyButton>`,
+  methods: { action: action('clicked') },
+});
 
 export const Text = () => ({
   components: { MyButton },
