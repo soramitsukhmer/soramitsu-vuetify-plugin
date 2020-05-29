5ee5de3221f8f2c15042cb04d601853aa4872a83
Socheat Sok <alex@socheat.net>
2020-05-29 10:51:32 +0700

    feat(storybook): install @storybook/addon-docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs/register',
-    '@storybook/addon-storysource'
+    '@storybook/addon-storysource',
+    '@storybook/addon-docs'
   ],
 };
